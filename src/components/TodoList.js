f80fc3a3c86f5089b0398c6553884503b052955c
kyr4601 <jane4545@naver.com>
2025-07-02 17:57:11 +0900

    refactor: 투두리스트 html 인라인 요소 변수로 분리

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,45 +2,47 @@ function TodoList({ container, state, toggleTodo, removeTodo, selectTodo }) {
   const ul = document.createElement('ul');
   container.appendChild(ul);
 
-  if (state.todos.length > 0) {
-    state.todos.forEach((todo) => {
-      const li = document.createElement('li');
-      li.innerHTML = `
-        <input type="checkbox" ${todo.id === state.editingId ? 'checked' : ''}/>
-        <span ${
-          todo.isCompleted ? 'style="text-decoration: line-through;"' : ''
-        }>${todo.name}</span>
+  if (state.todos.length === 0) return;
+
+  state.todos.forEach((todo) => {
+    const li = document.createElement('li');
+
+    const checked = todo.id === state.editingId ? 'checked' : '';
+    const completed = todo.isCompleted
+      ? 'style="text-decoration: line-through;"'
+      : '';
+    const completedIcon = todo.isCompleted
+      ? `<img src="src/images/check.svg" alt="check" />`
+      : '';
+
+    li.innerHTML = `
+        <input type="checkbox" ${checked}/>
+        <span ${completed}>${todo.name}</span>
         <div class="icons">
-          <div>
-          ${
-            todo.isCompleted
-              ? `<img src="src/images/check.svg" alt="check" />`
-              : ''
-          }
-         </div>
+          <div>${completedIcon}</div>
           <div class="delete-btn">
             <img src="src/images/delete.svg" alt="delete" />
           </div>
         </div>
       `;
-      // 할 일 완료 처리
-      li.querySelector('span').addEventListener('click', () => {
-        toggleTodo(todo.id);
-      });
-      // 할 일 삭제 처리
-      li.querySelector('.delete-btn').addEventListener('click', () => {
-        removeTodo(todo.id);
-      });
-      // 할 일 수정을 위한 개별 선택 처리
-      li.querySelector('input').addEventListener('change', () => {
-        if (state.editingId === todo.id) {
-          selectTodo(null);
-        } else {
-          selectTodo(todo.id);
-        }
-      });
-      ul.appendChild(li);
+    // 할 일 완료 처리
+    li.querySelector('span').addEventListener('click', () => {
+      toggleTodo(todo.id);
     });
-  }
+    // 할 일 삭제 처리
+    li.querySelector('.delete-btn').addEventListener('click', () => {
+      removeTodo(todo.id);
+    });
+    // 할 일 수정을 위한 개별 선택 처리
+    li.querySelector('input').addEventListener('change', () => {
+      if (state.editingId === todo.id) {
+        selectTodo(null);
+      } else {
+        selectTodo(todo.id);
+      }
+    });
+    ul.appendChild(li);
+  });
 }
+
 export default TodoList;
